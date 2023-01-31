a9348b7b782b54d6a30696b751e071593b4ee11b
adrocodes <adriaan.jvr2018@gmail.com>
2023-01-31 20:37:42 +1100

    feat: build mode event | indicator states

--- src/building.rs
+++ src/building.rs
@@ -45,7 +45,7 @@ impl Default for BuildingState {
 }
 
 // Start - Building Events
-pub struct BuildingModeChange(bool);
+pub struct BuildingModeChange(pub bool);
 pub struct TownCentreBuilt;
 // End - Building Events
 
@@ -62,8 +62,31 @@ impl Plugin for BuildingPlugin {
             .add_system_set(
                 SystemSet::on_update(GameState::Playing)
                     .with_system(BuildingIndicator::track_position)
-                    .with_system(BuildingIndicator::track_visibility),
+                    .with_system(BuildingIndicator::track_visibility)
+                    .with_system(BuildingPlugin::listen_build_mode_event)
+                    .with_system(BuildingPlugin::cancel_build_mode),
             );
+    }
+}
+
+impl BuildingPlugin {
+    fn listen_build_mode_event(
+        mut event: EventReader<BuildingModeChange>,
+        mut state: ResMut<BuildingState>,
+    ) {
+        if !event.is_empty() {
+            for change in event.iter() {
+                state.mode_active = change.0;
+            }
+        }
+
+        event.clear();
+    }
+
+    fn cancel_build_mode(keys: Res<Input<KeyCode>>, mut state: ResMut<BuildingState>) {
+        if keys.just_pressed(KeyCode::Escape) {
+            state.mode_active = false;
+        }
     }
 }
 
@@ -77,29 +100,44 @@ impl BuildingIndicator {
             SpriteBundle {
                 texture: textures.texture_selector.clone(),
                 transform: Transform::from_xyz(0.0, 0.0, 5.0),
+                visibility: Visibility { is_visible: false },
                 ..default()
             },
             Name::new("BuildingIndicator"),
-            // Visibility { is_visible: false },
         ));
     }
 
     fn track_position(
-        mut indicator_query: Query<&mut Transform, With<BuildingIndicator>>,
-        tile_query: Query<&Bounds2, With<Tile>>,
+        mut indicator_query: Query<(&mut Transform, &mut Handle<Image>), With<BuildingIndicator>>,
+        tile_query: Query<(&Bounds2, Option<&Buildable>), With<Tile>>,
         mouse: Res<MousePosition>,
+        textures: Res<TextureAssets>,
     ) {
-        let mut transform = indicator_query.single_mut();
+        let (mut transform, mut texture) = indicator_query.single_mut();
 
-        for bound in tile_query.iter() {
+        for (bound, buildable) in tile_query.iter() {
             if bound.in_bounds_centered(mouse.world) {
                 transform.translation = Vec3::new(bound.position.x, bound.position.y, 5.0);
+
+                match buildable {
+                    Some(_) => {
+                        *texture = textures.texture_selector.clone();
+                    }
+                    None => {
+                        *texture = textures.texture_selector_err.clone();
+                    }
+                };
+
                 return;
             }
         }
     }
 
-    fn track_visibility() {
-        // todo!();
+    fn track_visibility(
+        mut query: Query<&mut Visibility, With<BuildingIndicator>>,
+        state: Res<BuildingState>,
+    ) {
+        let mut visiblity = query.single_mut();
+        visiblity.is_visible = state.mode_active;
     }
 }
